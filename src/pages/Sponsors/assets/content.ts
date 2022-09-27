3817ab88d4fb17068f568a1f98f23fcb888f11f0
Raghav Khullar <khullarraghav2001@gmail.com>
2022-09-28 02:10:20 +0530

    feat(UI): seed data in sponsors page

---
+++ src/pages/Sponsors/assets/content.ts
@@ -0,0 +1,94 @@
+// import AGSCinemasOfficialLogo from "public/Assets/Images/SponsorImages/AGSCinemasOfficialLogo.png";
+// import ChennaiNews from "public/Assets/Images/SponsorImages/ChennaiNews.png";
+// import CinePartner
+// import DailyEducationalPartner
+// import DailyNewsPartner
+// import DigitalStreamingPartner
+// import EnjoymentPartner
+// import MediaPartner
+// import MusicStreamingPartner
+// import NammaMagazinePartner
+// import OfficialLifestylePartner
+// import PresentingPartner
+// import PressPartnerMakkalTV
+// import PressPartnerPuthiyaThalamurai
+// import PunPartner
+// import RadioPartner
+
+export const contentUp: any = [
+    {
+        companyName:"Chennaiyil.com",
+        src:"/Assets/Images/SponsorImages/ChennaiNews.png"
+    },
+    {
+        companyName:"AGS Cinemas",
+        src:"/Assets/Images/SponsorImages/AGSCinemasOfficialLogo.png"
+    },
+    {
+        companyName:"KalviMalar",
+        src:"/Assets/Images/SponsorImages/AGSCinemasOfficialLogo.png"
+    },
+    {
+        companyName:"Shortfundly",
+        src:"/Assets/Images/SponsorImages/AGSCinemasOfficialLogo.png"
+    },
+    {
+        companyName:"Mirror Now",
+        src:"/Assets/Images/SponsorImages/AGSCinemasOfficialLogo.png"
+    },
+    {
+        companyName:"Business Trichy",
+        src:"/Assets/Images/SponsorImages/AGSCinemasOfficialLogo.png"
+    },
+    {
+        companyName:"Woman's Era",
+        src:"/Assets/Images/SponsorImages/AGSCinemasOfficialLogo.png"
+    },
+    {
+        companyName:"Movies Now",
+        src:"/Assets/Images/SponsorImages/AGSCinemasOfficialLogo.png"
+    },
+    {
+        companyName:"Puthiya Thalamurai",
+        src:"/Assets/Images/SponsorImages/AGSCinemasOfficialLogo.png"
+    }
+];
+
+export const contentDown:any = [
+    {
+        companyName:"Makkal TV",
+        src:"/Assets/Images/SponsorImages/AGSCinemasOfficialLogo.png"
+    },
+    {
+        companyName:"HaramiPUN",
+        src:"/Assets/Images/SponsorImages/AGSCinemasOfficialLogo.png"
+    },
+    {
+        companyName:"Suryan FM",
+        src:"/Assets/Images/SponsorImages/AGSCinemasOfficialLogo.png"
+    },
+    {
+        companyName:"The Logical Indian",
+        src:"/Assets/Images/SponsorImages/AGSCinemasOfficialLogo.png"
+    },
+    {
+        companyName:"The Week",
+        src:"/Assets/Images/SponsorImages/AGSCinemasOfficialLogo.png"
+    },
+    {
+        companyName:"Youth Incorporated",
+        src:"/Assets/Images/SponsorImages/AGSCinemasOfficialLogo.png"
+    },
+    {
+        companyName:"Fever FM",
+        src:"/Assets/Images/SponsorImages/AGSCinemasOfficialLogo.png"
+    },
+    {
+        companyName:"Dinamalar",
+        src:"/Assets/Images/SponsorImages/AGSCinemasOfficialLogo.png"
+    },
+    {
+        companyName:"Awesong",
+        src:"/Assets/Images/SponsorImages/AGSCinemasOfficialLogo.png"
+    }
+];
